08df42bf911da41df6976d265662390c9d8ce5fb
skpayaya <sashi.kumar.patelskp@gmail.com>
2022-03-12 12:41:22 +0530

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { StrictMode } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <StrictMode>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </StrictMode>
+    );
 }
 
 export default App;
